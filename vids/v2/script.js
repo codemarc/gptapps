0af344df3ddd01fc8691d39afb5ad7ce48f95eac
marc j. greenberg <codemarc@gmail.com>
2024-11-12 10:02:41 -0500

    adding v2

---
+++ vids/v2/script.js
@@ -0,0 +1,18 @@
+document.getElementById('videoInput').addEventListener('change', function (e) {
+  const file = e.target.files[0]
+  const video = document.getElementById('videoPlayer')
+  const url = URL.createObjectURL(file)
+  video.src = url
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const video = document.getElementById('videoPlayer')
+  video.src = './ForRichie.mp4'
+  const fileInput = document.getElementById('videoInput')
+  fileInput.style.display = 'none'
+
+  video.play()
+  video.pl
+
+})
